4fbdd2209cf3a83c433aefd6977b7fa9076c42e1
Scott Motte <mot@mot.la>
2022-10-17 13:53:37 -0700

    Running on port

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
-const express = require('express')
+require('dotenv-vault').config()
 
+const PORT = process.env.PORT || 5000
+
+const express = require('express')
 const app = express()
 
-app.listen(process.env.PORT || 5000, () => {
-  console.log('Running.')
+app.listen(PORT, () => {
+  console.log(`Running on port ${PORT}.`)
 })
 
 app.get('/', (req, res) => {
-  res.send('Hello World')
+  res.send(`Hello ${process.env.HELLO}`)
 })
 
 // Export the Express API for Vercel
