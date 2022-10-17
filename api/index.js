9de99e4aa71c0ab42554a961d8dae5f34db80416
Scott Motte <mot@mot.la>
2022-10-17 13:34:33 -0700

    Initial commit

---
+++ api/index.js
@@ -0,0 +1,10 @@
+const app = require('express')()
+
+app.get('/api', (req, res) => {
+  res.setHeader('Content-Type', 'text/html')
+  res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate')
+
+  res.end(`Hello ${process.env.HELO}`)
+})
+
+module.exports = app
