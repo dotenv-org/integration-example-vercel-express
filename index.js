426c763249ed9e938873a0084a0fe5d9f92fb034
Scott Motte <mot@mot.la>
2022-10-17 13:48:36 -0700

    Changes

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ const express = require('express')
 
 const app = express()
 
-app.get('/', (req, res) => {
-  res.send('Hello World')
+app.listen(process.env.PORT || 5000, () => {
+  console.log('Running.')
 })
 
-app.listen(5000, () => {
-  console.log('Running on port 5000.')
+app.get('/', (req, res) => {
+  res.send('Hello World')
 })
 
 // Export the Express API for Vercel
